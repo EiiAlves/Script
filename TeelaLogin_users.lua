cfd8c83548978654b8eb5d8994d58836e44b595d
EiiAlves <123194811+EiiAlves@users.noreply.github.com>
2025-02-17 20:15:42 -0300

    Update TeelaLogin_users.lua

--- TeelaLogin_users.lua
+++ TeelaLogin_users.lua
@@ -1,6 +1,6 @@
-TeelaLogin_users={
-["usuario1"]="senha1"
-["novoUsuario"]="novaSenha"
-["usuario2"]="senha2"
-["78"]="99"
+TeelaLogin_users = {
+    ["usuario1"] = "senha1",
+    ["novoUsuario"] = "novaSenha",
+    ["usuario2"] = "senha2",
+    ["78"] = "99"
 }
