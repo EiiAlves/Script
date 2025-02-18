fb98031b1355af373a83ba8ff102b3b83d6b8c97
EiiAlves <123194811+EiiAlves@users.noreply.github.com>
2025-02-18 18:14:47 -0300

    Update TeelaLogin_users.lua

--- TeelaLogin_users.lua
+++ TeelaLogin_users.lua
@@ -1,7 +1,6 @@
-TeelaLogin_users={
-["novoUsuario"]="novaSenha",
-["usuario1"]="senha1",
-["usuario2"]="senha2",
-["78"]="99",
-["Admin"]="2104"
+TeelaLogin_users = {
+    ["usuario1"] = { senha = "senha1", expira_em = "01/01/2025" },
+    ["novoUsuario"] = { senha = "novaSenha", expira_em = "01/02/2025" },
+    ["usuario2"] = { senha = "senha2", expira_em = "01/03/2025" },
+    ["78"] = { senha = "99", expira_em = "01/04/2025" }
 }
