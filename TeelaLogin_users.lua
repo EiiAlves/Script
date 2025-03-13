86ace8f18a0c19eadbaea2cfa78ff6cd1fdb0a78
EiiAlves <123194811+EiiAlves@users.noreply.github.com>
2025-03-13 18:45:27 -0300

    Add files via upload

---
+++ TeelaLogin_users.lua
@@ -0,0 +1,6 @@
+TeelaLogin_users = {
+    ["usuario1"] = { senha = "senha1", expira_em = "01/01/2025" },
+    ["novoUsuario"] = { senha = "novaSenha", expira_em = "01/02/2025" },
+    ["usuario2"] = { senha = "senha2", expira_em = "01/03/2025" },
+    ["78"] = { senha = "99", expira_em = "01/04/2025" }
+}
